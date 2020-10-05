718aef3a941c95951bd6570f8314e758af3e8959
Om Jha <omjha23@gmail.com>
2020-10-05 09:11:30 -0400

    install vim-plug

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# install vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 # Create symlinks
 ln -sv ~/dotfiles/.zshrc ~
 ln -sv ~/dotfiles/.vimrc ~
